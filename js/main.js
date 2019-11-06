f605016abb33e9c38fa1923bd19d1b8b499f2785
eduardbin <binukeduard@gmail.com>
2019-11-06 17:52:59 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+; (function () {
+	"use strict";
+	let btn = document.querySelector('.ba-menu-toggle');
+	let menu = document.querySelector('.ba-menu');
+	Btn.addEventListener('click', function (e) {
+		menu.classList.toggle('open');
+	});
+})();
